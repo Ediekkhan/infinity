ec63ae7aabc5de30f8f25ba01e1952185ac80ca1
Ediekkhan <eddychristantus@gmail.com>
2023-02-02 15:30:18 +0100

    add to cart in progress

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
-function SellBoys(name, age, gender, lga) {
-    this.name= name ;
-    this.age= age;
-    this.gender= gender ;
-    this.lga= lga;
-    this.clean= function clean() {
-        alert("cleaning in progress")
-        
-    }
 
+const viewCart = document.getElementById('cart-modal');
+const addToCartBtn = document.getElementById('addCart');
+const product = document.getElementsByClassName('items');
+const revealCart = document.getElementById('cart');
+const hideCart= document.getElementById('closecart');  
+
+
+
+revealCart.addEventListener('click' , showCart);
+hideCart.addEventListener('click' , closeCart);
+
+function showCart() {
+  viewCart.style.visibility = 'visible';
 }
 
-var sellBoy1 = new SellBoys( "edi" , 12, "male", "eket")
-sellBoy1.clean()
+function closeCart() {
+  viewCart.style.visibility = 'hidden'; 
+}
 
 
-// var sellBoy5 = new SellBoys( "nda" , 34, "male", ["enugu" , "eket" ])
-// console.log(sellBoy5.lga)
+addToCartBtn.addEventListener('click' , function (e) {
+
+  // if (product.length) {
+  let cartItems = '';
+
+  //   product.forEach(() => {
+
+      cartItems = `
+
+      <div class=" d-flex justify-content-between align-content-center p-4" id="cart-item">
+      <img src="./img/gym_towel.webp" alt="gym_towel">
+      <h6 class="m-5">Sweat dapp</h6>
+      <p class="m-5">$45</p>
+
+      <div class="rm my-5">
+        <button class="btn btn-outline-dark">Remove</button>
+      </div>
+      </div>
+
+      `
+      
+  //   });
+
+    viewCart.innerHTML += cartItems;
+    
+  // }else {
+  //   viewCart.innerText = 'No item added to cart'
+  // }
+  
+})
